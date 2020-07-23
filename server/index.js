8dc24c685210296f5bcc056c9b0b1e545c55ce92
Aniket Kumar <aniketkumar049@gmail.com>
2020-07-23 17:41:04 +0530

    simple chat added

--- server/index.js
+++ server/index.js
@@ -4,6 +4,19 @@ var s = new server({ port : 5001})
 s.on('connection', function(ws){
   ws.on('message', function(message){
     console.log("Received: "+message);
-    ws.send(message);
+    
+    s.clients.forEach(function e(client) {
+      if(client != ws){
+        client.send(message);
+      }
+    })
+
+    // ws.send("From Server: "+message);
   })
-})
+
+  ws.on('close', function(){
+    console.log("I lost a client");
+  });
+
+  console.log("one more client connected");
+});
